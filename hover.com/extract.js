842f8f08f1a6559125e5aa668d168dfd1d7659eb
5Diraptor <5diraptor@gmail.com>
2021-10-08 10:30:34 +0100

    Create extract.js
    template to start work on

---
+++ hover.com/extract.js
@@ -0,0 +1,30 @@
+// CONFIG BEGIN
+var defaultTTL = 14400;
+// CONFIG END
+
+var table = document.getElementsByClassName('advanced_dns')[0];
+var rows = table.getElementsByTagName('tr');
+var i, len, row;
+var hostname, type, priority, ttl, destination;
+var output = '';
+
+output += '$TTL ' + defaultTTL + '\n'; // start with default TTL
+
+// skip header and last two rows (add new entry, delete all entries)
+for (i = 1, len = rows.length - 2; i < len; i++) {
+  row = rows[i];
+  hostname = row.getElementsByClassName('dns_hostname')[0].innerText;
+  type = row.getElementsByClassName('dns_type')[0].innerText;
+  priority = row.getElementsByClassName('dns_priority')[0].innerText;
+  ttl = row.getElementsByClassName('dns_ttl')[0].innerText || defaultTTL;
+  destination = row.getElementsByClassName('dns_data')[0].title;
+
+  if (type === 'TXT/SPF') {
+    type = 'TXT';
+    destination = '"' + destination + '"';
+  }
+
+  output += [hostname, ttl, 'IN', type, priority, destination].join(' ') + '\n';
+}
+
+console.log(output);
